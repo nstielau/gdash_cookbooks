f67c687356b68658a37c498951cdf455d3f454b7
Nick Stielau <nick.stielau@gmail.com>
2012-03-22 14:20:47 -0700

    Tweaks.

--- graphite/recipes/gdash.rb
+++ graphite/recipes/gdash.rb
@@ -26,7 +26,7 @@ template "/opt/gdash/config/gdash.yaml" do
 end
 
 template "/opt/gdash/config/demo_config.ru" do
-  source "config.ru..erb"
+  source "config.ru.erb"
   notifies :restart, "service[gdash.service]", :delayed
 end
 
