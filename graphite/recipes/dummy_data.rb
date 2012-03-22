9d38711d191d816595de5ee1236f38f39110d607
Nick Stielau <nick.stielau@gmail.com>
2012-03-22 11:24:20 -0700

    Tweaks.

---
+++ graphite/recipes/dummy_data.rb
@@ -0,0 +1,9 @@
+template "/usr/local/graphite_dummy_data.rb" do
+  mode "0755"
+  source "graphite_dummy_data.rb.erb"
+end
+
+cron "graphite_dummy_data" do
+  minute "*/5"
+  command "/usr/local/graphite_dummy_data.rb"
+end
